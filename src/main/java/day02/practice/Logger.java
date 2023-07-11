dc0fd2810353809152c7606e6e3e31f2e8b83035
al-sabana <118425884+al-sabana@users.noreply.github.com>
2023-07-11 17:04:28 +0530

    day02

--- src/main/java/day02/practice/Logger.java
+++ src/main/java/day02/practice/Logger.java
@@ -1,10 +1,29 @@
 package day02.practice;
 
 public class Logger {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void debug (String msg) {
+		System.out.println("DEBUG: " + msg);
 	}
-
+	public static void info (String msg) {
+		System.out.println("info : " + msg);
+	}
+	
+	public static void error (String msg) {
+		System.out.println("error: " + msg);
+	}
+	
+	public static void debug (int num) {
+		System.out.println("error: " + num);
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		debug("Please change this line");
+		info("update this code");
+		error("ArithmeticException");
+		debug(404);
+		
+		
+	}
 }
